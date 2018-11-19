3fe93bb4233bf012016f43fb1dd69866ad31770a
jiayusong <jiayusong2@creditease.cn>
2018-11-19 14:47:59 +0800

    增加图片引用

--- src/components/shared/LanguageSwitchBtn.js
+++ src/components/shared/LanguageSwitchBtn.js
@@ -4,11 +4,11 @@ import { connect } from "react-redux";
 import * as languageActionCreator from "../../actions/language";
 import { LANGUAGES } from "../../config/enum";
 import Menu from "antd/lib/menu";
-import "antd/lib/menu/style";
+import "antd/lib/menu/style/css";
 import Dropdown from "antd/lib/dropdown";
-import "antd/lib/dropdown/style";
+import "antd/lib/dropdown/style/css";
 import Icon from "antd/lib/icon";
-import "antd/lib/icon/style";
+import "antd/lib/icon/style/css";
 
 class LanguageSwitchBtn extends Component {
   render() {
